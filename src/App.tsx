218c333856d0d47bf13963f84c6614719201b9e9
Nikita <mnmforprog@gmail.com>
2024-03-14 15:33:45 +0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 import { useTelegram } from './hooks/useTelegram'
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
 		<div className={'wrapper'}>
 			<Header />
 			<Routes>
-				<Route index element={<div>TeleTeam - Telegram - WebApp</div>} />
+				<Route index element={<Main />} />
 				<Route path={'/form'} element={<div>Форма редактирования</div>} />
 			</Routes>
 		</div>
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+	return <div>TeleTeam - Telegram - WebApp</div>
+}
+
+export default Main
