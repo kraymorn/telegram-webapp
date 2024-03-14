aa5454f472cf6aad6655183e4ce359306c56fadc
Nikita <mnmforprog@gmail.com>
2024-03-14 12:37:31 +0300

    feat: added dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://kraymorn.github.io/telegram-webapp',
+	base: '/telegram-webapp/',
 })
